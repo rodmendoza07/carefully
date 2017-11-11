0d2e7f616433fd98d7de48089a4369cb87785129
Rodmendoza <lr.mendozar@gmail.com>
2017-11-11 10:17:23 -0600

    datatables active opcion

--- asset/customjs/therapiesPast.js
+++ asset/customjs/therapiesPast.js
@@ -1,11 +1,21 @@
 function therapiesPast () {
 	var that = this;
 
+	var objLanguage = new IdiomaDataTables();
+
 	this.LoadView = function () {
 
+		$("#sessions").addClass("active");
+		$("#sessionMenu").css("display",'block');
 		$("#info").load("views/client/therapiesPastA.html", function() {});
-		$("#content").load("views/client/therapiesPast.html", function(){});
-
+		
+		$("#content").load("views/client/therapiesPast.html", function(){
+			console.log($("#pastTable"));
+			$('#pastTable').DataTable({
+				"language": objLanguage.espanol,
+				"scrollX": true
+			});
+		});
 	}
 
 }
