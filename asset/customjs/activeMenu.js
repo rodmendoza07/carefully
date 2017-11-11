f96033dd557ecaa31f04763546f2bc16d71ed255
Rodmendoza <lr.mendozar@gmail.com>
2017-11-11 12:35:55 -0600

    console y espacios quitar de activeMenu

--- asset/customjs/activeMenu.js
+++ asset/customjs/activeMenu.js
@@ -2,21 +2,18 @@ function activeMenu () {
 	var that = this;
 
 	this.activate = function (id_selector, selectorChild, optionClick) {
-		//console.log($("#" + selector));
 		$("#" + id_selector).addClass("active");
 		$("#" + id_selector).css("background", "#8CC63F");
 		$("#" + selectorChild).css("display",'block');
 	}
 
 	this.emptyInfoMenu = function() {
-		console.log($(".active").data());
 		var deactive = $(".active").data("option");
-		console.log(deactive);
 		
 		$("#" + deactive).removeClass("active");
 		$("#" + deactive).removeAttr("style");
+		
 		$("#info").empty();
-
 		$("#content").empty();
 	}
 }
