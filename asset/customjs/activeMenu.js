6768650d37d1f241212ac9edcca12797c9e9fd25
rodrigo <luis.mendoza@grupoporvenir.com.mx>
2017-11-13 19:34:37 -0600

    imagen home

--- asset/customjs/activeMenu.js
+++ asset/customjs/activeMenu.js
@@ -2,9 +2,6 @@ function activeMenu () {
 	var that = this;
 
 	this.activate = function (id_selector, selectorChild) {
-		console.log(id_selector);
-		console.log(selectorChild);
-
 		$("#" + id_selector).addClass("active");
 		$("#" + id_selector).css("background", "#8CC63F");
 		$("#" + selectorChild).css("display",'block');
