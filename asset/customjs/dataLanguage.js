a26349cb7809eb29742caf67a5bcb6a2056ab765
Rodmendoza <lr.mendozar@gmail.com>
2017-11-16 21:45:49 -0600

    try catch functions js

--- asset/customjs/dataLanguage.js
+++ asset/customjs/dataLanguage.js
@@ -6,7 +6,7 @@ function IdiomaDataTables() {
 	    "sLengthMenu":     "Mostrar _MENU_ registros",
 	    "sZeroRecords":    "No se encontraron resultados",
 	    "sEmptyTable":     "Ningún dato disponible en esta tabla",
-	    "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+	    "sInfo":           "_TOTAL_ registros totales",
 	    "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
 	    "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
 	    "sInfoPostFix":    "",
