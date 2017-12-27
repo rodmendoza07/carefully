343fa0280520f1474600021bdf7847deeecc7d0e
Rodmendoza <lr.mendozar@gmail.com>
2017-12-27 05:56:59 -0600

    espacio menu izquierdo

--- register/asset/js/login.js
+++ register/asset/js/login.js
@@ -9,110 +9,77 @@ $(document).ready(function(){
             "Solo se permiten letras y espacios."
         );
 
-        $("#register").validate({
+        $("#formLogin").validate({
             rules: {
-                names: {
-                    required: true,
-                    lettersonly: true
-                },
-                lastnames: {
-                    required: true,
-                    lettersonly: true
-                },
-                pwd: {
-                    required: true,
-                    minlength: 6,
-                    maxlength: 10
-                },
-                cpwd: {
-                    required: true,
-                    equalTo: "#pwd"
-                },
-                userEmail: {
-                    required: true,
-                    email:true
-                },
-                disclaimer: {
-                    required: true
-                }
+                userName: "required",
+                passwd: "required"
             },
             messages: {
-                names: {
-                    required: "<br><span style='color: red;'>*</span>",
-                    lettersonly: "<br><br><span style='color: red;'>Solo se permiten letras y espacios.</span>"
-                },
-                lastnames:{
-                    required: "<br><span style='color: red;'>*</span>",
-                    lettersonly: "<br><br><span style='color: red;'>Solo se permiten letras y espacios.</span>"
-                },
-                pwd: "<br><span style='color: red;'>*</span>",
-                cpwd: {
-                    required: "<br><span style='color: red;'>*</span>",
-                    equalTo:"<br><br><span style='color: red;'>Las contraseñas no coinciden</span>"
-                },
-                userEmail: {
-                    required: "<br><span style='color: red;'>*</span>",
-                    email: "<br><br><span style='color: red;'>Ingresa un correo válido </span>"
-                },
-                disclaimer: "<br><span style='color: red; font-weight: bold;'>*</span>"
+                userName: '<br><span style="color:red;">*</span>',
+                passwd: '<br><span style="color:red;">*</span>'
             }
         });
 
-        $("#continueRegister").click(function(){
-            if ($("#register").valid()) {
-                var pwd = $("#pwd").val().trim();
-                var cpwd = $("#cpwd").val().trim();
-                var nombres = $("#names").val().trim();
-                var ap = $("#lastnames").val().trim();
-                var email = $("#userEmail").val().trim();
-                if (pwd != cpwd) {
-                    return toastr.error("La contraseñas no coinciden","¡Ups! Error");
-                }
-                
-                
-                var dataPost = {
-                    names: nombres,
-                    lastnames: ap,
-                    userEmail: email,
-                    pwd: pwd
-                };
-                
-                console.log(dataPost);
-                
-                var ajaxF = $.ajax({
-                    contentType: "application/json; charset=utf-8",
-                    type: "POST",
-                    url: "http://localhost:3000/newUsers",
-                    //url: "https://salty-harbor-47251.herokuapp.com/newUsers",
-                    data: dataPost,
-                    dataType: 'JSON',
-                        data: JSON.stringify(dataPost),
-                        beforeSend: function() {
-                            $('#loading').modal();
-                        },
-                        success: function (response) {
-                            $('#loading').modal('toggle');
-                            $("#names").val("");
-                            $("#lastnames").val("");
-                            $("#userEmail").val("");
-                            $("#pwd").val("");
-                            $("#cpwd").val("");
-                            $('#confirmEmail').modal();
-                            console.log(response);
-                        },
-                    error: function (XMLHttpRequest, textStatus, errorThrown){
-                        $('#loading').modal('toggle');
-                        $("#names").val("");
-                        $("#lastnames").val("");
-                        $("#userEmail").val("");
-                        $("#pwd").val("");
-                        $("#cpwd").val("");
-                        toastr.error("Error: " + errorThrown, "¡Atención!");
-                    }
-                });
-                console.log(ajaxF);
+        $("#formLogin").submit(function() {
+            if ($("#formLogin").valid()) {
+                console.log("submit");
             }
         });
+        // $("#continueRegister").click(function(){
+        //     if ($("#register").valid()) {
+        //         var pwd = $("#pwd").val().trim();
+        //         var cpwd = $("#cpwd").val().trim();
+        //         var nombres = $("#names").val().trim();
+        //         var ap = $("#lastnames").val().trim();
+        //         var email = $("#userEmail").val().trim();
+        //         if (pwd != cpwd) {
+        //             return toastr.error("La contraseñas no coinciden","¡Ups! Error");
+        //         }
+                
+                
+        //         var dataPost = {
+        //             names: nombres,
+        //             lastnames: ap,
+        //             userEmail: email,
+        //             pwd: pwd
+        //         };
+                
+        //         console.log(dataPost);
+                
+        //         var ajaxF = $.ajax({
+        //             contentType: "application/json; charset=utf-8",
+        //             type: "POST",
+        //             url: "http://localhost:3000/newUsers",
+        //             //url: "https://salty-harbor-47251.herokuapp.com/newUsers",
+        //             data: dataPost,
+        //             dataType: 'JSON',
+        //                 data: JSON.stringify(dataPost),
+        //                 beforeSend: function() {
+        //                     $('#loading').modal();
+        //                 },
+        //                 success: function (response) {
+        //                     $('#loading').modal('toggle');
+        //                     $("#names").val("");
+        //                     $("#lastnames").val("");
+        //                     $("#userEmail").val("");
+        //                     $("#pwd").val("");
+        //                     $("#cpwd").val("");
+        //                     $('#confirmEmail').modal();
+        //                     console.log(response);
+        //                 },
+        //             error: function (XMLHttpRequest, textStatus, errorThrown){
+        //                 $('#loading').modal('toggle');
+        //                 $("#names").val("");
+        //                 $("#lastnames").val("");
+        //                 $("#userEmail").val("");
+        //                 $("#pwd").val("");
+        //                 $("#cpwd").val("");
+        //                 toastr.error("Error: " + errorThrown, "¡Atención!");
+        //             }
+        //         });
+        //         console.log(ajaxF);
+        //     }
+        // });
 
         $("#activateSes").click(function(){
             $("#confirmEmail").modal("toggle");
