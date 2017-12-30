48b61518b7222babd10ac2038ccae98cdcb5f6dc
Rodmendoza <lr.mendozar@gmail.com>
2017-12-30 14:28:24 -0600

    validate login

--- register/asset/js/login.js
+++ register/asset/js/login.js
@@ -4,42 +4,62 @@ $(document).ready(function(){
     try {
         $("#formLogin").validate({
             rules: {
-                userName: "required",
-                passwd: "required"
+                userName: {
+                    required: true,
+                    email: true
+                },
+                passwd: {
+                    required: true,
+                    minlength: 6
+                }
             },
             messages: {
-                userName: '<br><span style="color:red;">*</span>',
-                passwd: '<br><span style="color:red;">*</span>'
+                userName: {
+                    required: "<br><span style='color: red; font-weight: bold;'>*</span>",
+                    email: "<br><br><span style='color: red;'>Ingresa un correo válido </span>"
+                },
+                passwd: '<br><span style="color:red; font-weight: bold;">*</span>'
             }
         });
 
         $("#startSess").click(function() {
             if ($("#formLogin").valid()) {
                 console.log("submit");
+                var response = "Activa tu cuenta para iniciar sesión"; 
                 var dataPost = {
                     userName: $("#userName").val().trim(),
                     passwd: $("#passwd").val().trim()
                 };
-                var ajaxF = $.ajax({
-                    contentType: "application/json; charset=utf-8",
-                    type: "POST",
-                    url: "http://localhost:3000/login",
-                    //url: "https://salty-harbor-47251.herokuapp.com/newUsers",
-                    //data: dataPost,
-                    dataType: 'JSON',
-                    data: JSON.stringify(dataPost),
-                    beforeSend: function() {
-                        $('#loading').modal();
-                    },
-                    success: function (response) {
-                        //$('#loading').modal('toggle');
-                        console.log(response);
-                    },
-                    error: function (XMLHttpRequest, textStatus, errorThrown){
-                        $('#loading').modal('toggle');
-                        toastr.error("Error: " + errorThrown, "¡Atención!");
-                    }
-                });
+                $("#showErr").append(
+                    '<div class="panel panel-danger">'
+                        + '<div class="panel-heading">'
+                            + '<span style="color: #fff;">'
+                                + '<i class="fa fa-exclamation-triangle" aria-hidden="true"></i>&nbsp;&nbsp;'
+                                + response
+                            + '</span>'
+                        + '</div>'
+                    + '</div>'
+                );
+                // var ajaxF = $.ajax({
+                //     contentType: "application/json; charset=utf-8",
+                //     type: "POST",
+                //     url: "http://localhost:3000/login",
+                //     //url: "https://salty-harbor-47251.herokuapp.com/newUsers",
+                //     //data: dataPost,
+                //     dataType: 'JSON',
+                //     data: JSON.stringify(dataPost),
+                //     beforeSend: function() {
+                //         $('#loading').modal();
+                //     },
+                //     success: function (response) {
+                //         //$('#loading').modal('toggle');
+                //         console.log(response);
+                //     },
+                //     error: function (XMLHttpRequest, textStatus, errorThrown){
+                //         $('#loading').modal('toggle');
+                //         toastr.error("Error: " + errorThrown, "¡Atención!");
+                //     }
+                // });
             }
         });
         // $("#continueRegister").click(function(){
