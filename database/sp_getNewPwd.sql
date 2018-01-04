4ca72576044ae1703ef60117c3dda919c0336cb2
rodrigo <luis.mendoza@grupoporvenir.com.mx>
2018-01-04 13:13:59 -0600

    sp_getnewpwd

--- database/sp_getNewPwd.sql
+++ database/sp_getNewPwd.sql
@@ -3,11 +3,16 @@ DROP procedure IF EXISTS `sp_getNewPwd`;
 
 DELIMITER $$
 USE `cuidadosamente`$$
-CREATE PROCEDURE `sp_getNewPwd` (
-	IN userMail VARCHAR(50)
+CREATE PROCEDURE `sp_getNewPwd`(
+    IN opt INT,
+	IN userMail VARCHAR(50),
+    IN pwdNew VARCHAR(15),
+    IN valHash_ VARCHAR(35)
 )
 BEGIN
 	DECLARE eCounter INT;
+    DECLARE userId INT;
+    DECLARE pwdHash VARCHAR(35);
     DECLARE `_rollback` BOOL DEFAULT 0;
     DECLARE EXIT HANDLER FOR SQLEXCEPTION
     BEGIN
@@ -18,12 +23,54 @@ BEGIN
     
     SET eCounter = (SELECT COUNT(*) FROM usuarios WHERE usr_correo = userMail);
     
-    IF eCounter > 0 THEN
-		SELECT * FROM usuarios;
-    ELSE
-		SIGNAL SQLSTATE '45000'
-			SET message_text =  'Cuenta inválida.';
-    END IF;
+    IF opt = -1 THEN
+    
+		IF eCounter > 0 THEN
+			
+            SET pwdHash = (SELECT md5(CONCAT(NOW(), userMail, (SELECT cfg_valor FROM configuraciones WHERE cfg_id = 1))));
+			SET userId = (SELECT usr_id FROM usuarios WHERE usr_correo = userMail);
+            
+			START TRANSACTION;
+			INSERT INTO newPwd (
+				np_usr_id
+				, np_hash
+			) VALUES (
+				userId
+				, pwdHash
+			);
+			IF `_rollback` THEN
+				SIGNAL SQLSTATE '45000'
+					SET message_text = 'Algo ha ido mal, intentalo más tarde.';
+			ELSE
+				COMMIT;
+			END IF;
+			
+		ELSE
+			SIGNAL SQLSTATE '45000'
+				SET message_text =  'Cuenta inválida.';
+		END IF;
+        
+	ELSEIF opt = -2 THEN
+		
+        SET userId = (SELECT np_usr_id FROM newPwd WHERE np_hash = valHash_);
+		
+        START TRANSACTION;
+		
+        UPDATE usuarios SET
+			usr_password = md5(CONCAT((SELECT usr_correo FROM usuarios WHERE usr_id = userId), pwdNew, (SELECT cfg_valor FROM configuraciones WHERE cfg_id = 1)))
+		WHERE usr_id = userId;
+        
+        UPDATE newPwd SET
+			np_status = 1
+		WHERE np_hash = valHash_;
+        
+        IF `_rollback` THEN
+			SIGNAL SQLSTATE '45000'
+				SET message_text = 'Algo ha ido mal, intentalo más tarde.';
+		ELSE
+			COMMIT;
+		END IF;
+	END IF;
 END$$
 
 DELIMITER ;
