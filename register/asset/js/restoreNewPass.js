058677432afe77fb437d8710bef89c48fa1c7dbb
rodmendoza <lr.mendozar@gmail.com>
2018-01-07 13:57:11 -0600

    update pass

--- register/asset/js/restoreNewPass.js
+++ register/asset/js/restoreNewPass.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     var that = this;
     try {
-        $("#restorePass").validate({
+        $("#restoreNewPass").validate({
             rules: {
                 newPasswd: {
                     required: true,
@@ -23,7 +23,30 @@ $(document).ready(function() {
         });
     
         $("#saveNewPass").click(function() {
-    
+            if ($("#restoreNewPass").valid) {
+                var newContra = {
+                    newPwd: $("#newPasswd").val().trim()
+                }
+                
+                var ajaxF = $.ajax({
+                    contentType: "application/json; charset=utf-8",
+                    type: "POST",
+                    url: "include/restorePassSend.php",
+                    dataType: 'JSON',
+                    data: JSON.stringify(newContra),
+                    beforeSend: function() {
+                        $('#loading').modal();
+                    },
+                    success: function (response) {
+                    },
+                    error: function (XMLHttpRequest, textStatus, errorThrown){
+                        $('#loading').modal('toggle');
+                        toastr.error("Algo ha ido mal, por favor intentalo más tarde.", "¡Atención!", 5000);
+                        console.log('RestoreNewPass - ', errorThrown);
+                        console.log('RestoreNewPass - ', XMLHttpRequest);
+                    }
+                });
+            }
         });
     } catch (x) {
         toastr.error("Algo ha ido mal, por favor intentalo más tarde.", "¡Atención!", 5000);
--- register/include/class/class.register.php
+++ register/include/class/class.register.php
@@ -85,5 +85,34 @@
                 echo json_encode($catch);
             }
         }
+
+        public function restoreNewPass($pwdNew, $valHash_){
+            try {
+                include 'class/connection.php';
+
+                $call = $conecta->prepare('CALL sp_getNewPwd(?,?,?,?)');
+                $opt = -2;
+                $userEmail = "";
+                $call->bind_param('isss', $opt, $userEmail, $pwdNew, $valHash_);
+                $call->execute();
+
+                if ($call->errno > 0) {
+                    $errno = $call->errno;
+                    $msg = $call->error;
+                    $resp = array('status' => 500, 'errno' => $errno, 'message' => utf8_encode($msg));
+                    echo json_encode($resp);
+                } else {
+                    $call->bind_result($hashVal);
+                    while ($call->fetch()) {
+                        $resp = array('status' => 200, 'data' => $hashVal);
+                        echo json_encode($resp);
+                    }
+                }
+
+            } catch (Exception $e) {
+                $catch = array('status' => 500, 'errno' => 1001, 'message' => $e);
+                echo json_encode($catch);
+            }
+        }
     }
 ?>
