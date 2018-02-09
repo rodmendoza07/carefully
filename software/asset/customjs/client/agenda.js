e18d5a8a1a0a5d67e1e715fd67caf5457000f252
rodmendoza <lr.mendozar@gmail.com>
2018-02-09 02:37:58 -0600

    prevent default

--- software/asset/customjs/client/agenda.js
+++ software/asset/customjs/client/agenda.js
@@ -6,136 +6,7 @@ function agenda() {
     this.start;
     this.end;
     this.comm;
-    this.viewAgenda = function() {
-        var successs = false;
-        $('#agenda').fullCalendar({
-            height : screen.height,
-            width  : screen.width,
-            header: {
-                left: 'prev,next today',
-                center: 'title',
-                right: 'month,agendaWeek,agendaDay'
-            },
-            defaultDate: moment.tz('America/Mexico_City'),
-            defaultView: 'agendaWeek',
-            scrollTime :  "8:00:00",
-            businessHours: {
-                dow: [1,2,3,4,5],
-                start: '8:00',
-                end: '20:00'
-            },
-            selectable: true,
-            select: function(start, end, jsEvent, view) {
-                //$('.myCheckbox').prop('checked', false);
-                console.log(moment(start).minutes());
-                var dayD = moment(start);
-                var startD = moment(start);
-                var endD = moment(end).add(20,'minutes');
-                var doctorD = "Sara Beneyto";
-                var textD = "¿Estás seguro que quieres agendar una cita con <span style='font-weight:bold;'>"
-                    + doctorD + "</span> el día <span style='font-weight:bold;'>" + dayD.format("DD/MM/YYYY") 
-                    + "</span> de <span style='font-weight:bold;'>" + startD.format("hh:mm:ss a") 
-                    + "</span> a <span style='font-weight:bold;'>" + endD.format("hh:mm:ss a") + "</span>?";
-                $("#agendadate").modal();
-                $("#datetitle").text("Nueva sesión");
-                $("#dateText").empty();
-                $("#dateText").append(textD);
 
-                $("#createSess").click(function() {
-                    that.start = '';
-                    that.end = '';
-                    console.log(that.start);
-                    console.log(that.end);
-                    console.log(newEvent);
-                    that.start = dayD.format('YYYY-MM-DD HH:mm:ss');
-                    that.end = endD.format('YYYY-MM-DD HH:mm:ss');
-                    var dataPost = {
-                        dStart: that.start, 
-                        dEnd: that.end,
-                        dCom: $('input[name=sessOpt]:checked', '#selectOpt').val()
-                    };
-                    var titleEvent = '';
-                    switch ($('input[name=sessOpt]:checked', '#selectOpt').val()) {
-                        case '1':
-                            titleEvent = 'Chat - Enviada';
-                            break;
-                        case '2':
-                            titleEvent = 'Videoconferencia - Enviada';
-                            break;
-                        default:
-                            break;
-                    }
-                    
-                    var newEvent = {
-                        title: titleEvent,
-                        start: that.start,
-                        end: that.end,
-                        color: '#29ABE2'
-                    }
-                    console.log(dataPost);
-                    console.log(newEvent);
-                    //$("#agenda").fullCalendar('renderEvent', newEvent);
-                    //$("#agenda").fullCalendar('refetchEvents');
-                    //onsole.log($("#agenda").fullCalendar('refetchEvents'));
-                    var ajaxF = $.ajax({
-                        contentType: "application/json; charset=utf-8",
-                        type: "POST",
-                        url: "../include/ca3e4974a8639906d8099f07c44b54ee.php",
-                        dataType: 'JSON',
-                        data: JSON.stringify(dataPost),
-                        async: false,
-                        beforeSend: function() {
-                           // $('#loading').modal();
-                        },
-                        success: function (response) {
-                           // $("#agenda").fullCalendar( 'destroy' );
-                            //$("#agenda").fullCalendar('render');
-                            //$('#loading').modal('toggle');
-                            //$("#agendadate").modal('toggle');
-                            console.log(response);
-                            //$("#agenda").fullCalendar('renderEvents', that.events);
-                            //that.LoadAgenda();
-                            if (response.errno) {   
-                                //alert("que pedo");
-                                console.log('Agenda - ',response.message)
-                                return toastr.error(response.message, "¡Upps!", 5000);
-                            } 
-                            location.reload();
-                            //else {
-                            //     successs = true;
-                            //     return successs;
-                            //     //$('#loading').modal('toggle');
-                            //     //$("#agenda").empty();
-                            //     // $("#agenda").fullCalendar('renderEvent', newEvent);
-                            //     // //$('#agenda').fullCalendar('destroy');
-                            //     // //$("#agenda").fullCalendar('rerenderEvents');
-                            //     // console.log(newEvent);
-                            //     // console.log($("#agenda"));
-                            //     //that.viewAgenda();
-                                
-                            //     //$("#agendadate").modal('toggle');
-                            //     that.events = [];
-                            // }
-                        },
-                        error: function (XMLHttpRequest, textStatus, errorThrown){
-                            //$('#loading').modal('toggle');
-                            console.log('getAllDates - ', errorThrown);
-                            console.log('getAllDates - ', XMLHttpRequest);
-                            return toastr.error("Algo ha ido mal, por favor intentalo más tarde.", "¡Atención!", 5000);
-                        }
-                    });
-                });
-            },
-            eventClick: function(calEvent, jsEvent, view) {
-
-                // alert('Event: ' + calEvent.title);
-                // alert('Coordinates: ' + jsEvent.pageX + ',' + jsEvent.pageY);
-                // alert('View: ' + view.name);
-            },
-            events: that.events
-        });
-        console.log(that.events);
-    }
     this.getEvents = function() {
         var ajaxF = $.ajax({
             contentType: "application/json; charset=utf-8",
@@ -171,9 +42,128 @@ function agenda() {
             }
         });
     }
-    this.saveEvents = function() {
+    this.saveEvents = function(dataPost) {
+        $("#agenda").fullCalendar( 'destroy' );
         
+        that.viewAgenda();
+        var ajaxF;
+        ajaxF = $.ajax({
+            contentType: "application/json; charset=utf-8",
+            type: "POST",
+            url: "../include/ca3e4974a8639906d8099f07c44b54ee.php",
+            dataType: 'JSON',
+            data: JSON.stringify(dataPost),
+            async: false,
+            beforeSend: function() {
+               // $('#loading').modal();
+            },
+            success: function (response) {
+               // $("#agenda").fullCalendar( 'destroy' );
+                //$("#agenda").fullCalendar('render');
+                //$('#loading').modal('toggle');
+                //$("#agendadate").modal('toggle');
+                console.log(response);
+                //$('#calendar').fullCalendar('addEventSource',events);
+                //$("#agenda").fullCalendar('renderEvent', newEvent);
+                //that.LoadAgenda();
+                // if (response.errno) {   
+                //     console.log('Agenda - ',response.message)
+                //     return toastr.error(response.message, "¡Upps!", 5000);
+                // } 
+                
+            },
+            error: function (XMLHttpRequest, textStatus, errorThrown){
+                //$('#loading').modal('toggle');
+                console.log('getAllDates - ', errorThrown);
+                console.log('getAllDates - ', XMLHttpRequest);
+                return toastr.error("Algo ha ido mal, por favor intentalo más tarde.", "¡Atención!", 5000);
+            }
+        });
     };
+
+
+    this.viewAgenda = function() {
+        that.getEvents();
+        var successs = false;
+        $('#agenda').fullCalendar({
+            height : screen.height,
+            width  : screen.width,
+            header: {
+                left: 'prev,next today',
+                center: 'title',
+                right: 'month,agendaWeek,agendaDay'
+            },
+            defaultDate: moment.tz('America/Mexico_City'),
+            defaultView: 'agendaWeek',
+            scrollTime :  "8:00:00",
+            businessHours: {
+                dow: [1,2,3,4,5],
+                start: '8:00',
+                end: '20:00'
+            },
+            selectable: true,
+            select: function(start, end, jsEvent, view) {
+                //$('.myCheckbox').prop('checked', false);
+                console.log(jsEvent);
+                var dayD = moment(start);
+                var startD = moment(start);
+                var endD = moment(end).add(20,'minutes');
+                var doctorD = "Sara Beneyto";
+                var textD = "¿Estás seguro que quieres agendar una cita con <span style='font-weight:bold;'>"
+                    + doctorD + "</span> el día <span style='font-weight:bold;'>" + dayD.format("DD/MM/YYYY") 
+                    + "</span> de <span style='font-weight:bold;'>" + startD.format("hh:mm:ss a") 
+                    + "</span> a <span style='font-weight:bold;'>" + endD.format("hh:mm:ss a") + "</span>?";
+                $("#agendadate").modal();
+                $("#datetitle").text("Nueva sesión");
+                $("#dateText").empty();
+                $("#dateText").append(textD);
+
+                $("#createSess").click(function(event) {
+                    event.preventDefault();
+                    that.start = '';
+                    that.end = '';
+                    that.start = dayD.format('YYYY-MM-DD HH:mm:ss');
+                    that.end = endD.format('YYYY-MM-DD HH:mm:ss');
+                    var dataPost = {
+                        dStart: that.start, 
+                        dEnd: that.end,
+                        dCom: $('input[name=sessOpt]:checked', '#selectOpt').val()
+                    };
+                    var titleEvent = '';
+                    switch ($('input[name=sessOpt]:checked', '#selectOpt').val()) {
+                        case '1':
+                            titleEvent = 'Chat - Enviada';
+                            break;
+                        case '2':
+                            titleEvent = 'Videoconferencia - Enviada';
+                            break;
+                        default:
+                            break;
+                    }
+                    
+                    var newEvent = {
+                        title: titleEvent,
+                        start: that.start,
+                        end: that.end,
+                        color: '#29ABE2'
+                    }
+                    console.log(dataPost);
+                    console.log(newEvent);
+                    that.saveEvents(dataPost);
+                });
+            },
+            eventClick: function(calEvent, jsEvent, view) {
+
+                // alert('Event: ' + calEvent.title);
+                // alert('Coordinates: ' + jsEvent.pageX + ',' + jsEvent.pageY);
+                // alert('View: ' + view.name);
+            },
+            events: that.events
+        });
+        console.log(that.events);
+        that.events = [];
+    }
+    
 	this.LoadAgenda = function() {
 		try {
 			$(".agenda").click(function() {
@@ -182,9 +172,9 @@ function agenda() {
 	
 				//$("#info").load("views/client/ticketCreatedCA.html", function() {});
 	
-				$("#content1").load("client/agenda.html", function() {
+				$("#content1").load("client/agenda.html", function(event) {
                     $("#frontAgenda").datepicker();
-                    that.getEvents();
+
                     that.viewAgenda();
                     
                     $("#cancelDc").click(function(event){
