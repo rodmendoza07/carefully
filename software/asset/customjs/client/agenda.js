a913f81371e5fa322b2babbb5971cc45d782b805
rodmendoza <lr.mendozar@gmail.com>
2018-02-09 03:19:53 -0600

    casi listo

--- software/asset/customjs/client/agenda.js
+++ software/asset/customjs/client/agenda.js
@@ -6,6 +6,7 @@ function agenda() {
     this.start;
     this.end;
     this.comm;
+    this.counter = false;
 
     this.getEvents = function() {
         var ajaxF = $.ajax({
@@ -42,12 +43,74 @@ function agenda() {
             }
         });
     }
-    this.saveEvents = function(dataPost) {
-        $("#agenda").fullCalendar( 'destroy' );
+    this.pmodal = function(dayD,startD, endD, doctorD) {
+        var textD = "¿Estás seguro que quieres agendar una cita con <span style='font-weight:bold;'>"
+            + doctorD + "</span> el día <span style='font-weight:bold;'>" + dayD.format("DD/MM/YYYY") 
+            + "</span> de <span style='font-weight:bold;'>" + startD.format("hh:mm:ss a") 
+            + "</span> a <span style='font-weight:bold;'>" + endD.format("hh:mm:ss a") + "</span>?";
+        $("#agendadate").modal();
+        $("#datetitle").text("Nueva sesión");
+        $("#dateText").empty();
+        $("#dateText").append(textD);
+    }
+    this.clickEvents = function(start, end, jsEvent, view){
         
-        that.viewAgenda();
-        var ajaxF;
-        ajaxF = $.ajax({
+        jsEvent.preventDefault();
+        console.log(that.counter);
+        var dayD = moment(start);
+        var startD = moment(start);
+        var endD = moment(end).add(20,'minutes');
+        var doctorD = "Sara Beneyto";
+
+        if (!that.counter) {
+            
+            
+            that.pmodal(dayD,startD, endD, doctorD);
+
+            $("#createSess").click(function(event) {
+                that.counter = true;
+                event.preventDefault();
+                that.start = '';
+                that.end = '';
+                that.start = dayD.format('YYYY-MM-DD HH:mm:ss');
+                that.end = endD.format('YYYY-MM-DD HH:mm:ss');
+                var dataPost = {
+                    dStart: that.start, 
+                    dEnd: that.end,
+                    dCom: $('input[name=sessOpt]:checked', '#selectOpt').val()
+                };
+                var titleEvent = '';
+                switch ($('input[name=sessOpt]:checked', '#selectOpt').val()) {
+                    case '1':
+                        titleEvent = 'Chat - Enviada';
+                        break;
+                    case '2':
+                        titleEvent = 'Videoconferencia - Enviada';
+                        break;
+                    default:
+                        break;
+                }
+                
+                var newEvent = {
+                    title: titleEvent,
+                    start: that.start,
+                    end: that.end,
+                    color: '#29ABE2'
+                }
+                that.saveEvents(dataPost, event);
+            });
+            } else {
+                console.log();
+                $("#agendadate").empty();
+                that.pmodal(dayD,startD, endD, doctorD);
+            }
+         //$('.myCheckbox').prop('checked', false);
+         
+    }
+    this.saveEvents = function(dataPost, event) {
+        console.log(event);
+        event.preventDefault();
+        var ajaxF = $.ajax({
             contentType: "application/json; charset=utf-8",
             type: "POST",
             url: "../include/ca3e4974a8639906d8099f07c44b54ee.php",
@@ -61,8 +124,10 @@ function agenda() {
                // $("#agenda").fullCalendar( 'destroy' );
                 //$("#agenda").fullCalendar('render');
                 //$('#loading').modal('toggle');
-                //$("#agendadate").modal('toggle');
+                $("#agendadate").modal('toggle');
                 console.log(response);
+                $("#agenda").fullCalendar( 'destroy' );
+                that.viewAgenda();
                 //$('#calendar').fullCalendar('addEventSource',events);
                 //$("#agenda").fullCalendar('renderEvent', newEvent);
                 //that.LoadAgenda();
@@ -102,55 +167,8 @@ function agenda() {
                 end: '20:00'
             },
             selectable: true,
-            select: function(start, end, jsEvent, view) {
-                //$('.myCheckbox').prop('checked', false);
-                console.log(jsEvent);
-                var dayD = moment(start);
-                var startD = moment(start);
-                var endD = moment(end).add(20,'minutes');
-                var doctorD = "Sara Beneyto";
-                var textD = "¿Estás seguro que quieres agendar una cita con <span style='font-weight:bold;'>"
-                    + doctorD + "</span> el día <span style='font-weight:bold;'>" + dayD.format("DD/MM/YYYY") 
-                    + "</span> de <span style='font-weight:bold;'>" + startD.format("hh:mm:ss a") 
-                    + "</span> a <span style='font-weight:bold;'>" + endD.format("hh:mm:ss a") + "</span>?";
-                $("#agendadate").modal();
-                $("#datetitle").text("Nueva sesión");
-                $("#dateText").empty();
-                $("#dateText").append(textD);
-
-                $("#createSess").click(function(event) {
-                    event.preventDefault();
-                    that.start = '';
-                    that.end = '';
-                    that.start = dayD.format('YYYY-MM-DD HH:mm:ss');
-                    that.end = endD.format('YYYY-MM-DD HH:mm:ss');
-                    var dataPost = {
-                        dStart: that.start, 
-                        dEnd: that.end,
-                        dCom: $('input[name=sessOpt]:checked', '#selectOpt').val()
-                    };
-                    var titleEvent = '';
-                    switch ($('input[name=sessOpt]:checked', '#selectOpt').val()) {
-                        case '1':
-                            titleEvent = 'Chat - Enviada';
-                            break;
-                        case '2':
-                            titleEvent = 'Videoconferencia - Enviada';
-                            break;
-                        default:
-                            break;
-                    }
-                    
-                    var newEvent = {
-                        title: titleEvent,
-                        start: that.start,
-                        end: that.end,
-                        color: '#29ABE2'
-                    }
-                    console.log(dataPost);
-                    console.log(newEvent);
-                    that.saveEvents(dataPost);
-                });
+            select: function(start, end, jsEvent, view){
+                that.clickEvents(start, end, jsEvent, view)
             },
             eventClick: function(calEvent, jsEvent, view) {
 
@@ -178,6 +196,7 @@ function agenda() {
                     that.viewAgenda();
                     
                     $("#cancelDc").click(function(event){
+                        event.preventDefault();
                         $("#chatC").iCheck('check');
                         $("#videoC").iCheck('uncheck');
                     });
