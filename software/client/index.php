254063084a134197bd06ae2c94826f603b9cae5e
rodmendoza <lr.mendozar@gmail.com>
2018-02-11 16:18:58 -0600

    modal

--- software/client/index.php
+++ software/client/index.php
@@ -222,7 +222,13 @@
     <div class="modal fade" id="mnewWarnings" role="dialog">
       <div class="modal-dialog modal-lg">
           <div class="modal-content">
-            <div class="modal-body text-center" id="detailWarning">
+            <div class="modal-header">
+              <label style="font-size: 24px;">¡Avisos!</label>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body" id="detailWarning">
               <div class="row">
                 <div class="col-md-12">
                   <table class='table table-striped table-bordered'>
