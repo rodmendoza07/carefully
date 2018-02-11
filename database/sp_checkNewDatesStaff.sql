3b157d4c4766d1946a105506845ae644e0798ac4
rodmendoza <lr.mendozar@gmail.com>
2018-02-11 00:59:31 -0600

    update de cita validation

--- database/sp_checkNewDatesStaff.sql
+++ database/sp_checkNewDatesStaff.sql
@@ -4,7 +4,9 @@ DROP procedure IF EXISTS `sp_checkNewDatesStaff`;
 DELIMITER $$
 USE `cuidadosamente`$$
 CREATE PROCEDURE `sp_checkNewDatesStaff`(
-	IN shash VARCHAR(35)
+	IN shash VARCHAR(35),
+    IN opt INT,
+    IN cId INT
 )
 BEGIN
 	DECLARE userId INT;
@@ -18,18 +20,38 @@ BEGIN
     
     SET userId = (SELECT vt_st_id FROM validtokens WHERE vt_hash = shash AND vt_status = 1);
     
-    SELECT
-		c.cita_fecha_start
-        , c.cita_fecha_end
-        , cc.cc_desc
-        , CONCAT(usr.usr_nombre, ' ', usr.usr_paterno) AS usrNombre
-    FROM citas c
-		INNER JOIN citas_communication cc ON (c.cita_title = cc.cc_id)
-        INNER JOIN usuarios usr ON (c.cita_paciente_id = usr_id)
-        INNER JOIN citas_validation cv ON (c.cita_id = cv.cv_c_id)
-    WHERE c.cita_doctor_id = userId
-     AND cv.cv_status = 0;
-    SELECT userId;
+    IF opt = 1 THEN
+		SELECT
+			c.cita_fecha_start
+			, c.cita_fecha_end
+			, cc.cc_desc
+			, CONCAT(usr.usr_nombre, ' ', usr.usr_paterno) AS usrNombre
+		FROM citas c
+			INNER JOIN citas_communication cc ON (c.cita_title = cc.cc_id)
+			INNER JOIN usuarios usr ON (c.cita_paciente_id = usr_id)
+			INNER JOIN citas_validation cv ON (c.cita_id = cv.cv_c_id)
+		WHERE c.cita_doctor_id = userId
+		 AND cv.cv_status = 0;
+	
+    ELSEIF opt = 2 THEN
+		START TRANSACTION;
+		
+        UPDATE citas_validation SET
+			cv_status = 1,
+            cv_validat = CURRENT_TIMESTAMP,
+            cv_st_id = userId
+		WHERE cv_c_id = cId;
+        
+        IF `_rollback` THEN
+			SIGNAL SQLSTATE '45000'
+				SET message_text = 'Algo ha ido mal, intentalo más tarde.';
+		ELSE
+			COMMIT;
+		END IF;
+    ELSE
+		SIGNAL SQLSTATE '45000'
+			SET message_text = 'Opción inválida';
+    END IF;
 END$$
 
 DELIMITER ;
