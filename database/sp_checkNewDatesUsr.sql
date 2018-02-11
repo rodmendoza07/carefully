4594415adffb31e0e92476e8074d9c86c864b762
rodmendoza <lr.mendozar@gmail.com>
2018-02-11 03:15:20 -0600

    cambios horarios

--- database/sp_checkNewDatesUsr.sql
+++ database/sp_checkNewDatesUsr.sql
@@ -3,7 +3,7 @@ DROP procedure IF EXISTS `sp_checkNewDatesUsr`;
 
 DELIMITER $$
 USE `cuidadosamente`$$
-CREATE PROCEDURE `sp_checkNewDatesUsr`(
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_checkNewDatesUsr`(
 	IN shash VARCHAR(35),
     IN opt INT,
     IN cId INT,
@@ -31,7 +31,8 @@ BEGIN
     
     IF opt = 1 THEN
 		SELECT
-			c.cita_fecha_start
+			c.cita_id
+            , c.cita_fecha_start
 			, c.cita_fecha_end
 			, cc.cc_desc
             , cst.cs_desc
@@ -47,59 +48,64 @@ BEGIN
 	
     ELSEIF opt = 2 THEN
 		
-        SET pacienteId = (SELECT cita_paciente_id WHERE cita_id = cId);
-        SET compareStart = (SELECT COUNT(*) FROM citas WHERE cita_paciente_id = pacienteId AND (cita_fecha_start BETWEEN dStart AND dEnd));
-        SET compareEnd = (SELECT COUNT(*) FROM citas WHERE cita_paciente_id = pacienteId AND (cita_fecha_end BETWEEN dStart AND dEnd));
+        SET pacienteId = (SELECT cita_paciente_id FROM citas WHERE cita_id = cId);
         SET ccStatus = (SELECT COUNT(*) FROM citas_status WHERE cs_id = cStatus);
-        SET dates = (SELECT DATEDIFF(dateStart, dateEnd));
+        
+		IF cStatus > 0 THEN
 		
-        IF compareStart = 0 && compareEnd = 0 && dates = 0 THEN
-			IF cStatus > 0 THEN
+			START TRANSACTION;
 			
-				START TRANSACTION;
+			UPDATE citas_validation SET
+				cv_status = 0,
+				cv_status_view = 0,
+				cv_validat = CURRENT_TIMESTAMP,
+				cv_usr_id = userId
+			WHERE cv_c_id = cId;
+			
+			IF cStatus = 4 THEN
+				UPDATE citas SET
+					cita_estatus = cStatus
+					, cita_usr_id_update = userId
+					, cita_usr_id_cancelacion = userId
+					, cita_fecha_update = CURRENT_TIMESTAMP
+					, cita_fecha_cancelacion = CURRENT_TIMESTAMP
+				WHERE cita_id = cId;
+			ELSEIF cStatus = 3 THEN 
 				
-				UPDATE citas_validation SET
-					cv_status = 0,
-					cv_status_view = 0,
-					cv_validat = CURRENT_TIMESTAMP,
-					cv_usr_id = userId
-				WHERE cv_c_id = cId;
-				
-				IF cStatus = 4 THEN
-					UPDATE citas SET
-						cita_estatus = cStatus
-						, cita_st_update = userId
-						, cita_st_id_cancelacion = userId
-						, cita_fecha_update = CURRENT_TIMESTAMP
-						, cita_fecha_cancelacion = CURRENT_TIMESTAMP
-					WHERE cita_id = cId;
-				ELSEIF cStatus = 3 THEN 
+				SET compareStart = (SELECT COUNT(*) FROM citas WHERE cita_paciente_id = pacienteId AND (cita_fecha_start BETWEEN dStart AND dEnd));
+				SET compareEnd = (SELECT COUNT(*) FROM citas WHERE cita_paciente_id = pacienteId AND (cita_fecha_end BETWEEN dStart AND dEnd));
+				SET dates = (SELECT DATEDIFF(dStart, dEnd));
+			
+				IF compareStart = 0 && compareEnd = 0 && dates = 0 THEN
 					UPDATE citas SET
 					cita_estatus = cStatus
-						, cita_st_update = userId
+						, cita_usr_id_update = userId
 						, cita_fecha_update = CURRENT_TIMESTAMP
 						, cita_fecha_start = dStart
 						, cita_fecha_end = dEnd
 					WHERE cita_id = cId;
 				ELSE
-					UPDATE citas SET
-						cita_estatus = cStatus
-						, cita_st_update = userId
-						, cita_fecha_update = CURRENT_TIMESTAMP
-					WHERE cita_id = cId;
-				End If;
-				
-				IF `_rollback` THEN
 					SIGNAL SQLSTATE '45000'
-						SET message_text = 'Algo ha ido mal, intentalo más tarde.';
-				ELSE
-					COMMIT;
-					SELECT 'OK' AS msg;
+						SET message_text = 'Horario inválido';
 				END IF;
 			ELSE
+				UPDATE citas SET
+					cita_estatus = cStatus
+					, cita_usr_id_update = userId
+					, cita_fecha_update = CURRENT_TIMESTAMP
+				WHERE cita_id = cId;
+			End If;
+			
+			IF `_rollback` THEN
 				SIGNAL SQLSTATE '45000'
-					SET message_text = 'Estatus no válido.';
+					SET message_text = 'Algo ha ido mal, intentalo más tarde.';
+			ELSE
+				COMMIT;
+				SELECT 'OK' AS msg;
 			END IF;
+		ELSE
+			SIGNAL SQLSTATE '45000'
+				SET message_text = 'Estatus no válido.';
 		END IF;
     ELSE
 		SIGNAL SQLSTATE '45000'
