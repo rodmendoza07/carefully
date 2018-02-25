f85238325c49a74cfe3dec5d95c3fe19d2599d42
rodmendoza <lr.mendozar@gmail.com>
2018-02-25 11:18:42 -0600

    expediente

---
+++ database/sp_getAllPatients.sql
@@ -0,0 +1,30 @@
+USE `cuidadosamente`;
+DROP procedure IF EXISTS `sp_getAllPatients`;
+
+DELIMITER $$
+USE `cuidadosamente`$$
+CREATE PROCEDURE `sp_getAllPatients`(
+	IN shash VARCHAR(35)
+)
+BEGIN
+	DECLARE userId INT;
+    DECLARE `_rollback` BOOL DEFAULT 0;
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+		SET `_rollback` = 1;
+		ROLLBACK;
+        RESIGNAL;
+    END;
+    
+    SET userId = (SELECT IFNULL(vt_st_id, 0) FROM validtokens WHERE vt_hash = shash AND vt_status = 1 AND vt_usr_id = 0);
+	SET userId = IFNULL(userId, -1);
+    
+    SELECT
+		u.usr_id
+		, CONCAT(u.usr_nombre, ' ', u.usr_paterno, ' ', u.usr_materno) AS patienName
+    FROM expedientepaciente e
+		INNER JOIN usuarios u ON (e.expP_paciente_id = u.usr_id)
+	WHERE e.expP_doctor_id = userId;
+END$$
+
+DELIMITER ;
--- software/include/class/class.mytherapy.php
+++ software/include/class/class.mytherapy.php
@@ -61,5 +61,34 @@
             }
         }
 
+        public function getAllPatients($token) {
+            try {
+                include 'connection.php';
+
+                $call = $conecta->prepare('CALL sp_getAllPatients(?)');
+                $call->bind_param('s', $token);
+                $call->execute();
+                $call->bind_result($uId, $uName);
+
+                if ($call->errno > 0) {
+                    $errno = $call->errno;
+                    $msg = $call->error;
+                    $resp = array('status' => 500, 'errno' => $errno, 'message' => utf8_encode($msg));
+                    echo json_encode($resp);
+                } else {
+                    $patients = array();
+                    while($call->fetch()){
+                        $aTempArray = array('uId' => $uId, 'uName' => $uName);
+                        array_push($patients, $aTempArray);
+                    }
+                    $resp = array('status' => 200, 'data' => $patients);
+                    echo json_encode($resp);
+                }
+
+            } catch (Exception $e) {
+                $catch = array('status' => 500, 'errno' => 1001, 'message' => $e);
+                echo json_encode($catch);
+            }
+        }
     }
 ?>
