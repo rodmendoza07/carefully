c7c26024acb675f30f4595199ceefc91503c0863
rodmendoza <lr.mendozar@gmail.com>
2018-05-02 04:48:13 -0500

    edit phase 1

--- admin/asset/customjs/therapistEdit.js
+++ admin/asset/customjs/therapistEdit.js
@@ -4,6 +4,39 @@ function therapistEdit() {
     var objLanguage = new IdiomaDataTables();
     var objActiveMenu = new activeMenu();
 
+    var columnas = [
+		{ title: 'Nombre', data: 'day'}
+		, {title: 'Estatus', data: 'horario'}
+	];
+
+    this.getTherapyE = function() {
+		var ajaxF = $.ajax({
+			contentType: "application/json; charset=utf-8",
+			type: "POST",
+			url: "include/a40f4decbdfc66f89706dd0ce8ebf610.php",
+			dataType: 'JSON',
+			beforeSend: function() {
+				$('#loading').modal();
+			},
+			success: function (response) {
+				$('#loading').modal('toggle');
+				var datosTabla = response.data;
+				$('#pastTable').DataTable({
+					"language": objLanguage.espanol,
+					"scrollX": true,
+					data: datosTabla,
+					columns: columnas
+				});
+			},
+			error: function (XMLHttpRequest, textStatus, errorThrown){
+				$('#loading').modal('toggle');
+				console.log('MiTerapia - ', errorThrown);
+				console.log('MiTerapia - ', XMLHttpRequest);
+				return toastr.error("Algo ha ido mal, por favor intentalo más tarde.", "¡Atención!", 5000);
+			}
+		});
+	}
+
     this.loadTherapist = function() {
         try {
             $(".therapistEdit").click(function() {
@@ -11,7 +44,7 @@ function therapistEdit() {
 				objActiveMenu.activate("therapist","therapistEdit");
 	
 				$("#content1").load("view/therapistEdit.html", function(){
-                    $('#creditTable').DataTable({
+                    $('#editTable').DataTable({
 						"language": objLanguage.espanol,
 						"scrollX": true
 					});
--- admin/include/class/class.therapist.php
+++ admin/include/class/class.therapist.php
@@ -29,5 +29,35 @@
                 echo json_encode($catch);
             }
         }
+
+        public function getAllTherapist($token) {
+            try {    
+                include 'connection.php';
+
+                $call = $conecta->prepare('CALL sp_getAllTherapist(?)');
+                $call->bind_param('s', $token);
+                $call->execute();
+                $call->bind_result($nameC, $tStatus);
+                if ($call->errno > 0) {
+                    $errno = $call->errno;
+                    $msg = $call->error;
+                    $resp = array('status' => 500, 'errno' => $errno, 'message' => utf8_encode($msg));
+                    echo json_encode($resp);
+                } else {
+                    $th = array();
+                    while ($call->fetch()) {
+                        $aTemp = array('nameC' => utf8_encode($nameC), 'tStatus' => $tStatus);
+                        array_push($th,$aTemp);
+                    }
+                    
+                    $resp = array('status' => 200, 'data' => $th);
+                    echo json_encode($resp);
+                }
+                $call->close();
+            } catch (Exception $e) {
+                $catch = array('status' => 500, 'errno' => 1001, 'message' => $e);
+                echo json_encode($catch);
+            }
+        }
     }
 ?>
