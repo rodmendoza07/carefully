d18d3f6905b547c7d9bb1299d03feac00cdd1f7a
rodmendoza07 <lr.mendozar@icloud.com>
2018-05-28 16:39:07 -0500

    Add files via upload

--- database/cuidadosamente.sql
+++ database/cuidadosamente.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 10-05-2018 a las 15:01:57
+-- Tiempo de generación: 28-05-2018 a las 23:34:35
 -- Versión del servidor: 10.1.29-MariaDB
 -- Versión de PHP: 7.2.0
 
